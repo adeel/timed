4619114dd6b6d93084e539415d2c05821e15907b
Adeel <adeel2@umbc.edu>
2009-07-10 02:44:57 -0400

    unnecessary Timer class

--- timed.py
+++ timed.py
@@ -21,21 +21,6 @@ def list_categories():
   if not categories:
     print "%s <category-name>" % (sys.argv[0])
 
-class Timer:
-  
-  def __init__(self, name):
-    self.name = name
-  
-  def start(self):
-    self.start = time.time()
-  
-  def stop(self):
-    if self.start:
-      self.elapsed = time.time() - self.start
-      self.start = None
-      return self.elapsed
-  
-
 class Log:
   
   source = os.path.expanduser('~/.timed')
