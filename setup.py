49fb53402d2406e326da3415afb8fa15659c4ee8
Adeel <adeel2@umbc.edu>
2009-07-10 23:55:43 -0400

    new version number

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from ez_setup import use_setuptools
 use_setuptools()
-from setuptools import setup, find_packages
+from setuptools import setup
 
 f = open('README')
 desc = f.read()
 f.close()
 
 setup(name='timed',
-      version='0.2',
+      version='0.3',
       description="command-line time tracking",
       long_description=desc,
       url='http://github.com/adeel/timed',
