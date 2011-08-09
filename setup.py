13034786f41729c612d33f63869fc521a5193b9e
Adeel Ahmad Khan <adeel@adeel.ru>
2011-08-09 09:41:44 -0400

    change license to MIT

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from distutils.core import setup
 
 setup(
   name='timed',
-  version='0.4',
-  description="command-line time tracking",
-  long_description=open('README').read(),
+  version='0.4.1',
+  description="command-line time tracker",
   url='http://adeel.github.com/timed',
   author='Adeel Ahmad Khan',
   author_email='adeel@adeel.ru',
   packages=['timed'],
   scripts=['bin/timed'],
   install_requires=['termcolor'],
-  license='BSD',
+  license='MIT',
   classifiers=[
     'Development Status :: 4 - Beta',
     'Environment :: Console',
-    'License :: OSI Approved :: BSD License',
-    'Natural Language :: English',
     'Operating System :: POSIX',
     'Topic :: Utilities',
   ]
